5f9cfe404d0b3a2198de2c7720ff49cc9e34b72a
Khoironi Kurnia Syah <khoironidev@gmail.com>
2022-01-30 01:12:01 +0800

    test: nothing

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -12,7 +12,7 @@ const detectgreet = require("../data/greets.json").greets.detector;
 module.exports = {
   name: "messageCreate",
   execute(message, client) {
-    // if (message.guild.id != 935198488151617596) return;
+    // if (message.guild.id !== "935198488151617596") return;
     if (message.author.bot || message.author.id == client.user.id) return;
     const content = message.content.toLowerCase();
     const filter = content.split(" ");
